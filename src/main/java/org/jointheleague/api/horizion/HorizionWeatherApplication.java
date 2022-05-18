f8e2d43db573c8dbcd7afd42584b137003216ed9
ryanodonnell2 <rpodonnell2012@gmail.com>
2022-05-17 19:09:14 -0700

    initial

---
+++ src/main/java/org/jointheleague/api/horizion/HorizionWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.horizion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizionWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizionWeatherApplication.class, args);
+	}
+
+}
